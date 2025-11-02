478c705a6a7aef09127793e3e3c1b0f979372cfc
Shakeeb Shaikh <eren.85yeager@gmail.com>
2025-11-02 19:38:12 +0530

    Create CountingSort.py

---
+++ Sorting/CountingSort.py
@@ -0,0 +1,13 @@
+def counting_sort(arr):
+    max_val = max(arr)
+    count = [0] * (max_val + 1)
+    for num in arr:
+        count[num] += 1
+
+    i = 0
+    for num in range(len(count)):
+        while count[num] > 0:
+            arr[i] = num
+            i += 1
+            count[num] -= 1
+    return arr
