d00bb5e163c6639bcb62b1365331f4cb4987d2e7
Shakeeb Shaikh <eren.85yeager@gmail.com>
2025-11-05 22:20:30 +0530

    Create MinHeap.py

---
+++ Tree/MinHeap.py
@@ -0,0 +1,50 @@
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+    
+    def parent(self, i):
+        return (i - 1) // 2
+    
+    def left_child(self, i):
+        return 2 * i + 1
+    
+    def right_child(self, i):
+        return 2 * i + 2
+    
+    def insert(self, val):
+        self.heap.append(val)
+        self._heapify_up(len(self.heap) - 1)
+    
+    def _heapify_up(self, i):
+        parent = self.parent(i)
+        if i > 0 and self.heap[i] < self.heap[parent]:
+            self.heap[i], self.heap[parent] = self.heap[parent], self.heap[i]
+            self._heapify_up(parent)
+    
+    def extract_min(self):
+        if not self.heap:
+            return None
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._heapify_down(0)
+        return root
+    
+    def _heapify_down(self, i):
+        min_idx = i
+        left = self.left_child(i)
+        right = self.right_child(i)
+        
+        if left < len(self.heap) and self.heap[left] < self.heap[min_idx]:
+            min_idx = left
+        if right < len(self.heap) and self.heap[right] < self.heap[min_idx]:
+            min_idx = right
+        
+        if min_idx != i:
+            self.heap[i], self.heap[min_idx] = self.heap[min_idx], self.heap[i]
+            self._heapify_down(min_idx)
+    
+    def get_min(self):
+        return self.heap[0] if self.heap else None
