fe3aa530d761d63ea95abaa2e1048ba9d591b951
Shakeeb Shaikh <eren.85yeager@gmail.com>
2025-11-08 13:12:05 +0530

    Create SudokuSolver(Backtracking).py

---
+++ Recursion/SudokuSolver(Backtracking).py
@@ -0,0 +1,39 @@
+def is_valid(board, r, c, num):
+    for i in range(9):
+        if board[r][i] == num or board[i][c] == num:
+            return False
+    br, bc = 3*(r//3), 3*(c//3)
+    for i in range(br, br+3):
+        for j in range(bc, bc+3):
+            if board[i][j] == num:
+                return False
+    return True
+
+def solve_sudoku(board):
+    for r in range(9):
+        for c in range(9):
+            if board[r][c] == 0:
+                for num in range(1,10):
+                    if is_valid(board, r, c, num):
+                        board[r][c] = num
+                        if solve_sudoku(board):
+                            return True
+                        board[r][c] = 0
+                return False
+    return True
+
+# Tests
+sample_board = [
+ [5,3,0,0,7,0,0,0,0],
+ [6,0,0,1,9,5,0,0,0],
+ [0,9,8,0,0,0,0,6,0],
+ [8,0,0,0,6,0,0,0,3],
+ [4,0,0,8,0,3,0,0,1],
+ [7,0,0,0,2,0,0,0,6],
+ [0,6,0,0,0,0,2,8,0],
+ [0,0,0,4,1,9,0,0,5],
+ [0,0,0,0,8,0,0,7,9],
+]
+solve_sudoku(sample_board)
+assert sample_board[0][2] == 4
+print("Sudoku solved:", sample_board)[web:33][web:27][web:39]
